c02b1decc4fbfda84fa3effefce9d05f3c1a60be
Peinilla <33536706+Peinilla@users.noreply.github.com>
2019-11-24 05:38:36 +0900

    라이플,샷건 발사구현 / 재장전 구현

---
+++ Defend_the_Farm/Assets/Scripts/GunManager_Slider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GunManager_Slider : MonoBehaviour
+{
+    public Slider slider;
+
+    private float gage;
+    private float time;
+    private void OnEnable()
+    {
+        gage = 0;
+        time = 0;
+        slider.value = 0;
+    }
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime / 1.5f; 
+        gage = Mathf.Lerp(0, 1, time);
+        if (gage < 1)
+        {
+            slider.value = gage;
+        }
+    }
+}
--- Defend_the_Farm/Assets/Scripts/Player_gunManager.cs
+++ Defend_the_Farm/Assets/Scripts/Player_gunManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
 
 public enum type { handgun = 0, rifle = 1, shotgun = 2 }
 
@@ -9,32 +11,68 @@ public class Player_gunManager : MonoBehaviour
 {
     public GameObject Bullet;
     public Transform FirePos;
+    public Text bulletUI_loaded;
+    public Text bulletUI_total;
+
 
     private GameObject gunContainer;
     private Animator gun_anim;
     private type myGun;
     private float delay;
     private bool isReady;
-    
+
+    private int bullet_Total;
+    private int bullet_Loaded;
+
+    private int default_Handgun = 6;
+    private int default_Rifle = 50;
+    private int default_Shotgun = 1;
+
+
     private void Awake()
     {
         gunContainer = GameObject.Find("R_hand_container").gameObject;
         gun_anim = gunContainer.GetComponent<Animator>();
-        myGun = type.shotgun;
+        myGun = type.handgun;
         delay = 0.8f;
+        bullet_Total = -1;
+        bullet_Loaded = default_Handgun;
         isReady = true;
+        bulletUI_loaded.GetComponent<Text>().text = bullet_Loaded + "";
+        bulletUI_total.GetComponent<Text>().text = "";
+        ChangeGun(type.shotgun);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        shoot();
+        if (Input.GetMouseButtonDown(0) && isReady)
+        {
+            if (bullet_Loaded != 0)
+            {
+                bullet_Loaded--;
+                bulletUI_loaded.GetComponent<Text>().text = bullet_Loaded + "";
+                shoot();
+                if(bullet_Loaded == 0 && bullet_Total == 0)
+                {
+                    ChangeGun(type.handgun);
+                }
+            }
+            else
+            {
+                reload();
+            }
+
+        }
+        if (Input.GetKey(KeyCode.R) && isReady)
+        {
+            reload();
+        }
     }
 
     void shoot()
     {
-        if (Input.GetMouseButtonDown(0) && isReady)
-        {
             gun_anim.Play("shoot");
             switch (myGun)
             {
@@ -61,32 +99,134 @@ public class Player_gunManager : MonoBehaviour
             }
             isReady = false;
             StartCoroutine(WaitForIt());
-        }
+      
 
     }
 
-    void ChangeGun(type t)
+    void ChangeGun(type gun)
     {
-        switch (t)
+        switch (gun)
         {
             case type.handgun:
                 myGun = type.handgun;
                 delay = 0.8f;
+                bullet_Total = -1;
+                bullet_Loaded = default_Handgun;
                 break;
             case type.rifle:
                 myGun = type.rifle;
                 delay = 0.1f;
+                bullet_Total = default_Rifle * 4;
+                bullet_Loaded = default_Rifle;
                 break;
             case type.shotgun:
                 myGun = type.shotgun;
                 delay = 1f;
+                bullet_Total = default_Shotgun * 1;
+                bullet_Loaded = default_Shotgun;
                 break;
         }
+        bulletUI_loaded.GetComponent<Text>().text = bullet_Loaded + "";
+        if (bullet_Total != -1)
+        {
+            bulletUI_total.GetComponent<Text>().text = bullet_Total + "";
+        }
+        else
+        {
+            bulletUI_total.GetComponent<Text>().text = "";
+        }
+    }
+
+    void reload()
+    {
+        if(bullet_Loaded == getMaxBullet(myGun)){ return; }
+
+        isReady = false;
+        //
+        // 리로드 효과음
+        StartCoroutine(WaitForReload());
+
     }
 
     IEnumerator WaitForIt()
     {
         yield return new WaitForSeconds(delay);
         isReady = true;
+    }
+
+    IEnumerator WaitForReload()
+    {
+        GameObject.Find("Canvas").transform.Find("Reload_Slider").gameObject.SetActive(true);
+        GameObject.Find("Canvas").transform.Find("Reload_Text").gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(1.5f);
+        
+        switch (myGun)
+        {
+            case type.handgun:
+                bullet_Loaded = default_Handgun;
+                break;
+            case type.rifle:
+                int r = default_Rifle - bullet_Loaded;
+                if (bullet_Total >= r)
+                {
+                    bullet_Total -= r;
+                    bullet_Loaded = default_Rifle;
+                }
+                else
+                {
+                    bullet_Loaded += bullet_Total;
+                    bullet_Total = 0;
+                }
+                break;
+            case type.shotgun:
+                int s = default_Shotgun - bullet_Loaded;
+                if (bullet_Total >= s)
+                {
+                    bullet_Total -= s;
+                    bullet_Loaded = default_Shotgun;
+                }
+                else
+                {
+                    bullet_Loaded += bullet_Total;
+                    bullet_Total = 0;
+                }
+                break;
+        }
+
+        bulletUI_loaded.GetComponent<Text>().text = bullet_Loaded + "";
+        if (bullet_Total != -1)
+        {
+            bulletUI_total.GetComponent<Text>().text = bullet_Total + "";
+        }
+        else
+        {
+            bulletUI_total.GetComponent<Text>().text = "";
+        }
+
+        GameObject.Find("Canvas").transform.Find("Reload_Slider").gameObject.SetActive(false);
+        GameObject.Find("Canvas").transform.Find("Reload_Text").gameObject.SetActive(false);
+
+        isReady = true;
+    }
+
+    int getMaxBullet(type gun)
+    {
+        int r = 0;
+
+        switch (gun)
+        {
+            case type.handgun:
+                r = default_Handgun;
+                break;
+            case type.rifle:
+                r = default_Rifle;
+                break;
+            case type.shotgun:
+                r = default_Shotgun;
+                break;
+        }
+
+        return r;
     }
 }

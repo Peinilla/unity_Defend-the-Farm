1e2336ed47fc0bc38d92f2731d875fb2315d96c6
Peinilla <33536706+Peinilla@users.noreply.github.com>
2019-11-24 07:40:16 +0900

    좀비 공격 수정 / 공격당할시 출혈 이미지 추가

---
+++ Defend_the_Farm/Assets/Scripts/Monster.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public int hp = 1;
+    public float attackDelay = 0.05f;
+
+    private GameObject target;
+    private Vector3 direction;
+    private float velocity;
+    private bool isMoveable;
+    private bool isPlayAttack;
+    private bool isAttackable;
+    private Rigidbody rb;
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMoveable = true;
+        target = GameObject.FindWithTag("Player");
+        rb = gameObject.GetComponent<Rigidbody>();
+        anim = transform.Find("Z_Model").gameObject.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = (target.transform.position - transform.position).normalized;
+        velocity = 1f * Time.smoothDeltaTime;
+        float distance = Vector3.Distance(target.transform.position, transform.position);
+
+        if (distance <= 30.0f && isMoveable)
+        {
+            anim.SetBool("isWalk", true);
+            this.transform.position = new Vector3(transform.position.x + (direction.x * velocity),
+                                                   transform.position.y + (direction.y * velocity),
+                                                     transform.position.z+ (direction.z * velocity));
+            Vector3 FixedPos =
+               new Vector3(
+               target.transform.position.x,
+               target.transform.position.y,
+               target.transform.position.z);
+            transform.LookAt(FixedPos);
+
+        }
+        else{
+            anim.SetBool("isWalk", false);
+
+            Vector3 FixedPos =
+               new Vector3(
+               target.transform.position.x,
+               target.transform.position.y,
+               target.transform.position.z);
+            transform.LookAt(FixedPos);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            hp--;
+            if(hp == 0)
+            {
+                //dead
+                anim.SetBool("isDead", true);
+                isMoveable = false;
+                Destroy(gameObject, 1);
+            }
+            else
+            {
+                //damaged
+            }
+
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (!isPlayAttack)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                anim.Play("Z_attack_A");
+                isMoveable = false;
+                isAttackable = true;
+                isPlayAttack = true;
+                StartCoroutine("attack");
+                StartCoroutine("attackDone");
+                //Attack
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isAttackable = false;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        rb.velocity = Vector3.zero;
+    }
+
+    IEnumerator attack()
+    {
+        yield return new WaitForSeconds(attackDelay);
+        if (isAttackable)
+        {
+            Debug.Log("aaaaa");
+            GameObject.FindWithTag("Player").gameObject.GetComponent<Player_state>().damaged();
+        }
+    }
+
+    IEnumerator attackDone()
+    {
+        yield return new WaitForSeconds(1.0f);
+        isPlayAttack = false;
+        if (!isAttackable)
+        {
+            isMoveable = true;
+        }
+    }
+}
---
+++ Defend_the_Farm/Assets/Scripts/Player_state.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_state : MonoBehaviour
+{
+
+    public float damagedDelay = 2.0f;
+
+    private Image img;
+    private Rigidbody rb;
+    private float time;
+    private float alpa;
+    private bool isDamaged;
+    private bool isDelay;
+
+    void Start()
+    {
+        img = GameObject.Find("UI_Damaged").gameObject.GetComponent<Image>();
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (isDamaged)
+        {
+            time += Time.deltaTime / 1.5f;
+            alpa = Mathf.Lerp(1, 0, time);
+            img.color = new Color(1, 1, 1, alpa);
+        }   
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        rb.velocity = Vector3.zero;
+
+        if (collision.gameObject.tag == "Monster")
+        {
+            if (!isDamaged)
+            {
+                damaged();
+            }
+        }
+    }
+
+    public void damaged()
+    {
+        if (!isDelay)
+        {
+            StartCoroutine("damaged_modifyUI");
+        }
+
+    }
+
+    IEnumerator damaged_modifyUI()
+    {
+        isDelay = true;
+
+        time = 0;
+        isDamaged = true;
+        img.color = new Color(1, 1, 1, 1);
+        yield return new WaitForSeconds(1.2f);
+        isDamaged = false;
+        img.color = new Color(1, 1, 1, 0);
+
+        yield return new WaitForSeconds(damagedDelay - 1.2f);
+        isDelay = false;
+    }
+}
